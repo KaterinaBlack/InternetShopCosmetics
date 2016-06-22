8e581500e969cc2e4188f47f3c4e982375aa4125
Kate Kondrashova <Kate Kondrashova>
2016-06-22 14:59:37 +0300

    Добавила вывод информации по продуктам в действие Index Home-контроллера в виде карточного инлайн интерфейса

--- InternetShopCosmetics/Models/Product.cs
+++ InternetShopCosmetics/Models/Product.cs
@@ -7,5 +7,15 @@ namespace InternetShopCosmetics.Models
 {
     public class Product
     {
+        public int Id { get; set; }    // id товара
+
+        public string Name { get; set; }   // название товара
+
+        public string Description { get; set; }   // описание товара
+        
+        //public string Text { get; set; }
+
+        public int Price { get; set; }        // цена товара
+
     }
 }
